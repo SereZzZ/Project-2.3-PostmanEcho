1dc73b0e946ae3d41ed447c1ab8386f714a43d32
Neto sergey test <serez.gavriliv@mail.ru>
2024-04-19 11:16:04 +0300

    Initial

---
+++ src/test/java/ru/netology/PostmanApiTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanApiTest {
+    @Test
+    public void shouldGiveSentData (){
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", Matchers.equalTo("data"))
+        ;
+    }
+}
